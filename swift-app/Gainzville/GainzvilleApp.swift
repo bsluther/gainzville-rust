4222051320d0189ddaa375d33f976e1c7340cd37
Brian Luther <briansluther@gmail.com>
2026-04-09 11:28:30 -0600

    Init xcode project.

---
+++ swift-app/Gainzville/GainzvilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GainzvilleApp.swift
+//  Gainzville
+//
+//  Created by Brian Luther on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GainzvilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-app/GainzvilleTests/GainzvilleTests.swift
@@ -0,0 +1,17 @@
+//
+//  GainzvilleTests.swift
+//  GainzvilleTests
+//
+//  Created by Brian Luther on 4/9/26.
+//
+
+import Testing
+@testable import Gainzville
+
+struct GainzvilleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
